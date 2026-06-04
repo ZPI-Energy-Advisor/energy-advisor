a22e8ae20ff2a6709d00e62eda17280fa7ec1db9
liwia dzikuć <liwiaa7@gmail.com>
2026-06-04 14:47:15 +0200

    [ZPI-13] [ ZPI-17]: endpoint uploadu + przeliczenie cen dla taryf i zapis do bazy (#12)
    * [ZPI-13] [ ZPI-17]: endpoint uploadu + przeliczenie cen dla taryf i zapis do bazy
    
    * fix: dodanie POSTGRES_HOST do pipeline

---
+++ backend/app/controllers/upload.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException, Depends
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.models.models import Simulation, User
+from app.services.calculations import calculate_all_tariffs
+
+router = APIRouter(
+    prefix="/upload",
+    tags=["Upload & Simulation"]
+)
+
+@router.post("/")
+async def upload_energy_data(
+    file: UploadFile = File(...), 
+    db: Session = Depends(get_db)
+):
+    if not file.filename.endswith('.csv'):
+        raise HTTPException(status_code=400, detail="Wymagany plik .csv")
+
+    simulation_results = calculate_all_tariffs(file.file, db)
+
+    dummy_user = db.query(User).first()
+    if not dummy_user:
+        dummy_user = User(email="test@advisor.pl")
+        db.add(dummy_user)
+        db.commit()
+        db.refresh(dummy_user)
+
+    new_simulation = Simulation(
+        user_id=dummy_user.id,
+        results=simulation_results
+    )
+    
+    db.add(new_simulation)
+    db.commit()
+    db.refresh(new_simulation)
+
+    return {
+        "status": "success",
+        "simulation_id": str(new_simulation.id),
+        "file_name": file.filename,
+        "summary": simulation_results,
+        "message": "Symulacja dla wszystkich taryf zakończona i zapisana w bazie!"
+    }
+
